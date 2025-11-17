b4029c7b7d5d63c61063cf0dfb7c98d4db34ec46
GitHub Action <action@github.com>
2025-11-17 23:40:09 +0000

    Auto-format code with Black

--- wahoo/__init__.py
+++ wahoo/__init__.py
@@ -1,6 +1,10 @@
 from .validator.api.client import ValidationAPIClient, ValidationAPIError
 from .validator.models import PerformanceMetrics, ValidationRecord
-from .validator.dataframe import records_to_dataframe, ensure_required_columns, flatten_record
+from .validator.dataframe import (
+    records_to_dataframe,
+    ensure_required_columns,
+    flatten_record,
+)
 from .validator.scoring.operators import Operator, OperatorResult, VolumeProfitOperator
 from .validator.scoring.pipeline import OperatorPipeline
 
